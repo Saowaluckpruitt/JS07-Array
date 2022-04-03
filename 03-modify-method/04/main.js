6b1eae065ac0480fd3175110dc34532a802a0045
Saowaluckpruitt <102138233+Saowaluckpruitt@users.noreply.github.com>
2022-04-03 15:50:01 +0700

    done

--- 03-modify-method/04/main.js
+++ 03-modify-method/04/main.js
@@ -1,7 +1,8 @@
 const first = (arr, n) => {
   if (n === "undefined" || n === 1) {
-    return arr[0];
+    return arr.find((el) => el[1]);
   } else {
-    return;
+    return arr.slice(0, n);
   }
 };
+console.log(first([7, 8, 0]));
